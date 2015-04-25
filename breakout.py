a926d692b35b2e189a82e1868737bcd45c251d31
Sean Sponsler <seaneezer@yahoo.com>
2015-04-25 11:48:59 -0700

    v3

--- breakout.py
+++ breakout.py
@@ -14,7 +14,7 @@ TODO
 
 
 
-replay=1
+
 #setting colors
 
 screenW=800
@@ -49,11 +49,11 @@ columnCounter=0
 outerListItem=-1
 innerListItem=-1
 levelMap1=[
-[0,0,0,1,1,1,0,0,1,1,1,1,0,0,0,1,1,0,0,0,1,0,0,0,1,0,0,0,0,0,0,0],
-[0,0,1,0,0,0,0,0,1,0,0,0,0,0,1,0,0,1,0,0,1,1,0,0,1,0,0,0,0,0,0,0],
-[0,0,1,1,1,0,0,0,1,1,1,1,0,0,1,1,1,1,0,0,1,0,1,0,1,0,0,0,0,0,0,0],
-[0,0,0,0,0,1,0,0,1,0,0,0,0,0,1,0,0,1,0,0,1,0,0,1,1,0,0,0,0,0,0,0],
-[0,0,1,1,1,0,0,0,1,1,1,1,0,0,1,0,0,1,0,0,1,0,0,0,1,0,0,0,0,0,0,0]
+    [0,0,0,1,1,1,0,0,1,1,1,1,0,0,0,1,1,0,0,0,1,0,0,0,1,0,0,0,0,0,0,0],
+    [0,0,1,0,0,0,0,0,1,0,0,0,0,0,1,0,0,1,0,0,1,1,0,0,1,0,0,0,0,0,0,0],
+    [0,0,1,1,1,0,0,0,1,1,1,1,0,0,1,1,1,1,0,0,1,0,1,0,1,0,0,0,0,0,0,0],
+    [0,0,0,0,0,1,0,0,1,0,0,0,0,0,1,0,0,1,0,0,1,0,0,1,1,0,0,0,0,0,0,0],
+    [0,0,1,1,1,0,0,0,1,1,1,1,0,0,1,0,0,1,0,0,1,0,0,0,1,0,0,0,0,0,0,0]
 ]
 
 highScore=0
@@ -63,92 +63,92 @@ print(highScoreLoad)
 #creating the block class
 
 class Block(pygame.sprite.Sprite):
-def __init__(self,power,x,y):
-    super(Block,self).__init__()
-    self.power=power
-    if power=='alien':
-        self.image=pygame.image.load(alien_brick_image_filename).convert()
-    else:
-        self.image=pygame.image.load(brick_image_filename).convert()
-    self.rect=self.image.get_rect()
-    self.rect.x=x
-    self.rect.y=y
+    def __init__(self,power,x,y):
+        super(Block,self).__init__()
+        self.power=power
+        if power=='alien':
+            self.image=pygame.image.load(alien_brick_image_filename).convert()
+        else:
+            self.image=pygame.image.load(brick_image_filename).convert()
+        self.rect=self.image.get_rect()
+        self.rect.x=x
+        self.rect.y=y
 
 class Ball(pygame.sprite.Sprite):
-#properties of the ball before __init__
-speed=10.0
-x=screenW/2
-y=screenH/2
-direction=190
-width=10
-height=10
-#creating template for instances of Ball
-def __init__(self):
-    super(Ball,self).__init__()
-    self.image=pygame.image.load(ball_image_filename).convert_alpha()
-    self.rect=self.image.get_rect()
-    self.screenheight=pygame.display.get_surface().get_height()
-    self.screenwidth=pygame.display.get_surface().get_width()
-#making the ball bounce when the direction is manipulated
-def bounce(self,diff):
-    #switching the direction of the ball when you bounce
-    self.direction=(180 - self.direction)%360
-    self.direction-=diff
+    #properties of the ball before __init__
+    speed=10.0
+    x=screenW/2
+    y=screenH/2
+    direction=190
+    width=10
+    height=10
+    #creating template for instances of Ball
+    def __init__(self):
+        super(Ball,self).__init__()
+        self.image=pygame.image.load(ball_image_filename).convert_alpha()
+        self.rect=self.image.get_rect()
+        self.screenheight=pygame.display.get_surface().get_height()
+        self.screenwidth=pygame.display.get_surface().get_width()
+    #making the ball bounce when the direction is manipulated
+    def bounce(self,diff):
+        #switching the direction of the ball when you bounce
+        self.direction=(180 - self.direction)%360
+        self.direction-=diff
 
-def update(self):
-    direction_radians=math.radians(self.direction)
+    def update(self):
+        direction_radians=math.radians(self.direction)
 
-    self.x +=self.speed * math.sin(direction_radians)
-    self.y -=self.speed * math.cos(direction_radians)
-    #setting the positions of the rectangles(x and y)?
-    self.rect.x=self.x
-    self.rect.y=self.y
-    #when the ball reaches y=0, the ball will bounce
+        self.x +=self.speed * math.sin(direction_radians)
+        self.y -=self.speed * math.cos(direction_radians)
+        #setting the positions of the rectangles(x and y)?
+        self.rect.x=self.x
+        self.rect.y=self.y
+        #when the ball reaches y=0, the ball will bounce
 
-    if self.y <=0:
-        self.bounce(0)
-        self.y=1
+        if self.y <=0:
+            self.bounce(0)
+            self.y=1
 
-    #horizontal direction swap?
-    if self.x <=0:
-        self.direction=(360-self.direction)%360
-        self.x=1
+        #horizontal direction swap?
+        if self.x <=0:
+            self.direction=(360-self.direction)%360
+            self.x=1
 
-    if self.x > self.screenwidth - self.width:
-        self.direction = (360 - self.direction)%360
-        self.x=self.screenwidth - self.width - 1
+        if self.x > self.screenwidth - self.width:
+            self.direction = (360 - self.direction)%360
+            self.x=self.screenwidth - self.width - 1
 
-    #if y exceeds the resolution of the game screen
-    if self.y>screenH:
-        return True
-    else:
-        return False
-#configuring paddle
+        #if y exceeds the resolution of the game screen
+        if self.y>screenH:
+            return True
+        else:
+            return False
+    #configuring paddle
 
 class Player(pygame.sprite.Sprite):
-def __init__(self):
-    super(Player,self).__init__()
-    #properties of the paddle
-    self.width=75
-    self.height=15
-    self.image=pygame.Surface([self.width,self.height])
-    self.image.fill((paddle_color))
-    #creating a rectangle with a position
-    self.rect = self.image.get_rect()
-    self.screenheight=pygame.display.get_surface().get_height()
-    self.screenwidth=pygame.display.get_surface().get_width()
-    #placing the rectangle at the bottom of the screen?
-    self.rect.x=0
-    self.rect.y=self.screenheight - self.height
-#updating the position of the paddle when the mouse moves
-def update(self,color):
-    self.image.fill(color)
-    pos=pygame.mouse.get_pos()
-    self.rect.x=pos[0]
-    if self.rect.x>self.screenwidth-self.width:
-        self.rect.x = self.screenwidth-self.width
-def color_change(self,color):
-    player.image.fill(color)
+    def __init__(self):
+        super(Player,self).__init__()
+        #properties of the paddle
+        self.width=75
+        self.height=15
+        self.image=pygame.Surface([self.width,self.height])
+        self.image.fill((paddle_color))
+        #creating a rectangle with a position
+        self.rect = self.image.get_rect()
+        self.screenheight=pygame.display.get_surface().get_height()
+        self.screenwidth=pygame.display.get_surface().get_width()
+        #placing the rectangle at the bottom of the screen?
+        self.rect.x=0
+        self.rect.y=self.screenheight - self.height
+    #updating the position of the paddle when the mouse moves
+    def update(self,color):
+        self.image.fill(color)
+        pos=pygame.mouse.get_pos()
+        self.rect.x=pos[0]
+        if self.rect.x>self.screenwidth-self.width:
+            self.rect.x = self.screenwidth-self.width
+    def color_change(self,color):
+        player.image.fill(color)
 
 pygame.init()
 screen=pygame.display.set_mode([screenW,screenH])
@@ -162,8 +162,6 @@ gameOverBackground=pygame.image.load(game_over_image_filename).convert_alpha()
 
 
 blocks = pygame.sprite.Group()
-superBlocks = pygame.sprite.Group()
-goodBlock=pygame.sprite.Group()
 balls=pygame.sprite.Group()
 allSprites=pygame.sprite.Group()
 #object player is being created and added to allSprites
@@ -181,23 +179,23 @@ blockCount=32
 #creating blocks in rows(5) and columns within the rows
 
 while rowCounter < len(levelMap1):
-columnCounter=0
-while columnCounter < 32:
-    outerListItem=levelMap1[rowCounter]
-    innerListItem=outerListItem[columnCounter]
-    if innerListItem ==0:
-        bPower='normal'
-        block=Block(bPower,columnCounter*(block_width+2)+2,top)
-        blocks.add(block)
-        allSprites.add(block)
-    elif innerListItem == 1:
-        bPower='alien'
-        block=Block(bPower, columnCounter * (block_width + 2)+2,top)
-        blocks.add(block)
-        allSprites.add(block)
-    columnCounter+=1
-top += block_height
-rowCounter+=1
+    columnCounter=0
+    while columnCounter < 32:
+        outerListItem=levelMap1[rowCounter]
+        innerListItem=outerListItem[columnCounter]
+        if innerListItem ==0:
+            bPower='normal'
+            block=Block(bPower,columnCounter*(block_width+2)+2,top)
+            blocks.add(block)
+            allSprites.add(block)
+        elif innerListItem == 1:
+            bPower='alien'
+            block=Block(bPower, columnCounter * (block_width + 2)+2,top)
+            blocks.add(block)
+            allSprites.add(block)
+        columnCounter+=1
+    top += block_height
+    rowCounter+=1
 
 
 clock=pygame.time.Clock()
@@ -210,61 +208,59 @@ game_over=False
 deadBlockCounter=0
 exit_program=False
 font=pygame.font.Font(None,32)
+
 while exit_program != True:
-clock.tick(30)
-screen.blit(background, (0,0))
-for event in pygame.event.get():
-    #if you quit the game via window closure, exit_program is TRUE
-    if event.type ==pygame.QUIT:
-        exit_program=True
+    clock.tick(30)
+    screen.blit(background, (0,0))
+    for event in pygame.event.get():
+        #if you quit the game via window closure, exit_program is TRUE
+        if event.type ==pygame.QUIT:
+            exit_program=True
 
-if not game_over:
+    if not game_over:
+        player.update(pColor)
+        game_over=ball.update()
+        score=font.render("Score: " + str(deadBlockCounter),True, (255,255,255))
+        scorePos=screenW-100,20
+        screen.blit(score,scorePos)
 
-    player.update(pColor)
-    game_over=ball.update()
-    score=font.render("Score: " + str(deadBlockCounter),True, (255,255,255))
-    scorePos=screenW-100,20
-    screen.blit(score,scorePos)
+    if deadBlockCounter > highScoreLoad:
+        highScore=deadBlockCounter
+        pickle.dump(highScore,open('save.p','wb'))
 
-if deadBlockCounter > highScoreLoad:
-    highScore=deadBlockCounter
-    pickle.dump(highScore,open('save.p','wb'))
+    highScoreText=font.render("High Score: "+str(highScoreLoad),True, (255,255,255))
+    highScorePos=screenW/2,20
+    screen.blit(highScoreText,highScorePos)
 
-highScoreText=font.render("High Score: "+str(highScoreLoad),True, (255,255,255))
-highScorePos=screenW/2,20
-screen.blit(highScoreText,highScorePos)
+    if game_over:
+        screen.blit(gameOverBackground,(screenW/2-200,screenH/2-50))
+        pickle.dump(highScore,open('save.p','wb'))
 
+    if pygame.sprite.spritecollide(player,balls,False):
+        diff=(player.rect.x + player.width/2)-(ball.rect.x + ball.width/2)
 
+        ball.rect.y=screen.get_height() - player.rect.height-ball.rect.height-1
+        ball.bounce(diff)
 
+    #when the sprites of (ball) and (block) collide, deadBlocks will become True
+    deadBlocks=pygame.sprite.spritecollide(ball,blocks,True)
 
-if game_over:
-    screen.blit(gameOverBackground,(screenW/2-200,screenH/2-50))
-    pickle.dump(highScore,open('save.p','wb'))
+    #the ball will not bounce on dead blocks
+    if len(deadBlocks)>0:
+        s=pygame.mixer.Sound('hit.wav')
+        s.play()
+        for b in deadBlocks:
+            if b.power == 'alien':
+                pColor=paddle_color2
+            else:
+                pColor=paddle_color
+        deadBlockCounter+= len(deadBlocks)
+        ball.bounce(0)
 
-if pygame.sprite.spritecollide(player,balls,False):
-    diff=(player.rect.x + player.width/2)-(ball.rect.x + ball.width/2)
+        if len(blocks)==0:
+            game_over=True
 
-    ball.rect.y=screen.get_height() - player.rect.height-ball.rect.height-1
-    ball.bounce(diff)
+    allSprites.draw(screen)
+    pygame.display.flip()
 
-#when the sprites of (ball) and (block) collide, deadBlocks will become True
-deadBlocks=pygame.sprite.spritecollide(ball,blocks,True)
-
-#the ball will not bounce on dead blocks
-if len(deadBlocks)>0:
-    s=pygame.mixer.Sound('hit.wav')
-    s.play()
-    for b in deadBlocks:
-        if b.power == 'alien':
-            pColor=paddle_color2
-        else:
-            pColor=paddle_color
-    deadBlockCounter+= len(deadBlocks)
-    ball.bounce(0)
-
-    if len(blocks)==0:
-        game_over=True
-
-allSprites.draw(screen)
-pygame.display.flip()
 pygame.quit()
